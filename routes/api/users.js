d6f0aa7a1ad5f036d135b54cb00d1c2f0bd42ac5
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-08-08 17:53:27 -0400

    Catch up

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,11 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const passport = require('passport');
+
+// Load input validation
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // load user model
 const User = require('../../models/User');
@@ -20,10 +25,18 @@ router.get('/test', (req, res) => res.json({msg: 'Users Works'}));
 // @desc register the user
 // @access Public
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check validation
+  if(!isValid) {
+    return res.status(400).json(errors) ;
+  }
+
   User.findOne({ email: req.body.email })
   .then(user => {
     if(user) {
-      return res.status(400).json({email: 'Email already exists'});
+      errors.email = 'Email already taken';
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: '200',  // size
@@ -56,6 +69,14 @@ router.post('/register', (req, res) => {
 // @desc login the user / returning the token
 // @access Public
 router.post('/login', (req,res) => {
+
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  //Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
@@ -64,8 +85,11 @@ router.post('/login', (req,res) => {
   .then(user => {
     // Check for User
     if (!user) {
-      return res.status(404).json({email: 'User not found'});
+      errors.email = 'Email not found';
+      return res.status(404).json(errors);
     }
+
+
     // Check password
     bcrypt.compare(password, user.password)
       .then(isMatch => {
@@ -84,10 +108,26 @@ router.post('/login', (req,res) => {
               });
             });
         } else {
-          return res.status(400).json({password: 'Password incorrect'});
+          errors.password = 'Password incorrect';
+          return res.status(400).json(errors);
         }
       });
   });
+})
+
+
+// @route GET api/users/current
+// @desc Return current user
+// @access Private
+router.get(
+  '/current',
+  passport.authenticate('jwt',  {session: false}),
+  (req,res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
 })
 
 

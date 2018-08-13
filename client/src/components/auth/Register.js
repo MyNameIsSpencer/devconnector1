032f553d4bd04c9a29fe4bf31952e27f9467a88e
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-08-13 19:02:08 -0400

    reset?

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,9 +1,13 @@
+<<<<<<< HEAD
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import classnames from 'classnames';
 import { connect } from 'react-redux';
 import { registerUser } from '../../actions/authActions';
+=======
+import React, { Component } from 'react'
+>>>>>>> parent of 16f4e91... implement classnames, especially for errors on register form, fix axios to client only
 
 class Register extends Component {
   constructor(props){
@@ -38,22 +42,25 @@ class Register extends Component {
 
   onSubmit(e) {
     e.preventDefault();
-
+    
     const newUser = {
       name: this.state.name,
       email: this.state.email,
       password: this.state.password,
       password2: this.state.password2,
+<<<<<<< HEAD
     };
 
     this.props.registerUser(newUser, this.props.history);
+=======
+    }
+    console.log(newUser);
+>>>>>>> parent of 16f4e91... implement classnames, especially for errors on register form, fix axios to client only
   }
 
   render() {
-    const { errors } = this.state;
-    // const errors = this.state.errors;
-
     return(
+<<<<<<< HEAD
       <div className="register">
         <div className="container">
           <div className="row">
@@ -81,6 +88,30 @@ class Register extends Component {
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
             </div>
+=======
+    <div className="register">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 m-auto">
+            <h1 className="display-4 text-center">Sign Up</h1>
+            <p className="lead text-center">Create your DevConnector account</p>
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group">
+                <input type="text" className="form-control form-control-lg" placeholder="Name" name="name" value={this.state.name} onChange={this.onChange}/>
+              </div>
+              <div className="form-group">
+                <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" value={this.state.email} onChange={this.onChange}/>
+                <small className="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
+              </div>
+              <div className="form-group">
+                <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" value={this.state.password} onChange={this.onChange}/>
+              </div>
+              <div className="form-group">
+                <input type="password" className="form-control form-control-lg" placeholder="Confirm Password" name="password2" value={this.state.password2} onChange={this.onChange}/>
+              </div>
+              <input type="submit" className="btn btn-info btn-block mt-4" />
+            </form>
+>>>>>>> parent of 16f4e91... implement classnames, especially for errors on register form, fix axios to client only
           </div>
         </div>
       </div>

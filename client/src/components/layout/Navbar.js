e99fe173457d1b08e0ac82af1be0ba60181625ad
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-08-17 01:20:02 -0400

    add postActions and postForm

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -25,6 +25,11 @@ class Navbar extends Component {
           </Link>
         </li>
         <li className="nav-item">
+          <Link className="nav-link" to='/feed'>
+            Post Feed
+          </Link>
+        </li>
+        <li className="nav-item">
           <a href="#" onClick={this.onLogoutClick.bind(this)} className="nav-link">
             <img
               className="rounded-circle"

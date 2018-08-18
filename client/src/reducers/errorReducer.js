59f55695d58c85a00dfa4d0c6c0fb4bd9aa5d34a
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-08-17 23:13:02 -0400

    allow for commenting on feeds and deleting comments

--- client/src/reducers/errorReducer.js
+++ client/src/reducers/errorReducer.js
@@ -1,4 +1,4 @@
-import { GET_ERRORS } from '../actions/types';
+import { GET_ERRORS, CLEAR_ERRORS } from '../actions/types';
 
 const initialState = {};
 
@@ -7,89 +7,9 @@ export default function(state = initialState, action) {
   switch (action.type) {
     case GET_ERRORS:
       return action.payload;
+    case CLEAR_ERRORS:
+      return {};
     default:
       return state;
   }
 }
-
-
-
-//
-//
-// export const registerUser = userData => dispatch => {
-//
-//   // history.push('/login'))
-//     axios
-//       .post('/api/users/register', userData)
-//       .then(res => console.log(res.data))
-//       .catch(err =>
-//         dispatch({
-//           type: GET_ERRORS,
-//           payload: err.response.data
-//         })
-//       );
-// };
-
-
-
-
-// import setAuthToken from '../utils/setAuthToken';
-// import jwt_decode from 'jwt-decode';
-//
-// import { GET_ERRORS, SET_CURRENT_USER } from './types';
-//
-// // Register User
-// export const registerUser = (userData, history) => dispatch => {
-//   axios
-//     .post('/api/users/register', userData)
-//     .then(res => history.push('/login'))
-//     .catch(err =>
-//       dispatch({
-//         type: GET_ERRORS,
-//         payload: err.response.data
-//       })
-//     );
-// };
-//
-//
-// // Login - Get User Token
-// export const loginUser = userData => dispatch => {
-//   axios
-//     .post('/api/users/login', userData)
-//     .then(res => {
-//       // Save to localStorage
-//       const { token } = res.data;
-//       // Set token to ls
-//       localStorage.setItem('jwtToken', token);
-//       // Set token to Auth header
-//       setAuthToken(token);
-//       // Decode token to get user data
-//       const decoded = jwt_decode(token);
-//       // Set current user
-//       dispatch(setCurrentUser(decoded));
-//     })
-//     .catch(err =>
-//       dispatch({
-//         type: GET_ERRORS,
-//         payload: err.response.data
-//       })
-//     );
-// };
-//
-// // Set logged in user
-// export const setCurrentUser = decoded => {
-//   return {
-//     type: SET_CURRENT_USER,
-//     payload: decoded
-//   };
-// };
-//
-// // Log user out
-// export const logoutUser = () => dispatch => {
-//   // Remove token from localStorage
-//   localStorage.removeItem('jwtToken');
-//   //Remove auth header for future requests
-//   setAuthToken(false);
-//   // Set current user to {} which will set isAuthenticated to false
-//   dispatch(setCurrentUser({}));
-// };
